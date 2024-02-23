8c353dde89b5ebdee09d918bb87d9fa9fb03b486
Ayesha Nazim <ayeshasidd422@gmail.com>
2024-02-23 20:45:47 +0500

    customer theme updation

--- Restaurant_App/views.py
+++ Restaurant_App/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from .models import Branch,Food_Item,Restaurant
 from .forms import BranchForm, FoodItemForm,RestaurantForm
 from Admin_App.models import Food_Category
@@ -116,13 +117,17 @@ def food_item_update(request, id):
             obj = Food_Item.objects.get(food_item_id = id)
         except Food_Item.DoesNotExist:
             return redirect('food_item_list')
-        form = FoodItemForm(request.POST or None, files=request.FILES,instance=obj)
-        if form.is_valid():
-            form.save()
-            return redirect('food_item_list')
+        if request.method=="POST":
+            form = FoodItemForm(request.POST or None, files=request.FILES,instance=obj)
+            if form.is_valid():
+                form.save()
+                return redirect('food_item_list')
+        form = FoodItemForm()
         return render(request, 'Restaurant_App/template/food_item/food_item_update.html', {'form':form,'food_item':obj,'cat':Food_Category.objects.all()})
     else:
-        return redirect('restaurant_signin')
+        return redirect("restaurant_signin")
+    
+        
 
 def food_item_delete(request, id):
     id = int(id)
